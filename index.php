bbbc8d1518457b9ad3f6780491dd09c4110b7d26
Andrew Louis <andrew.louis93@gmail.com>
2014-01-19 10:05:44 -0500

    Modified modal loading bar

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-body">
+			<br>
             <!-- Meat of the progress bar -->
                   <div class="progress progress-striped active">
                     <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
